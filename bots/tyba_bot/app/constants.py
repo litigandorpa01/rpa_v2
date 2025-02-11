7f00e1a1f1cd1389a5e93635f7383df899b5ebbf
Alejandro E Linux <alejandroecheverria.log@gmail.com>
2025-02-11 17:00:47 -0500

    Agregar configuración dotenv y variables de entorno (CAPMONSTER_KEY, WEBSITE_URL y WEBSITE_KEY)

---
+++ bots/tyba_bot/app/constants.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#KEYS
+CAPMONSTER_KEY = os.getenv("CAPMONSTER_KEY")
+
+#URLS
+WEBSITE_URL = os.getenv("WEBSITE_URL")
+WEBSITE_KEY = os.getenv("WEBSITE_KEY")
+
+
+
+
