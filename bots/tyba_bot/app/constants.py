d0d8e3f8a05898123436edd0abc94fa7f78382b3
Alejandro E Linux <alejandroecheverria.log@gmail.com>
2025-02-12 21:12:18 -0500

    Se agregan variables de entorno relacionadas a Rabbitmq

--- bots/tyba_bot/app/constants.py
+++ bots/tyba_bot/app/constants.py
@@ -9,7 +9,12 @@ CAPMONSTER_KEY = os.getenv("CAPMONSTER_KEY")
 #URLS
 WEBSITE_URL = os.getenv("WEBSITE_URL")
 WEBSITE_KEY = os.getenv("WEBSITE_KEY")
+PAGE_ACTION= os.getenv("PAGE_ACTION")
 
+#RABBIT
+RABBITMQ_HOST = os.getenv("RABBITMQ_HOST")
+QUEUE_NAME = os.getenv("QUEUE_NAME")
+PREFETCH_COUNT = int(os.getenv("PREFETCH_COUNT"))
 
 
 
