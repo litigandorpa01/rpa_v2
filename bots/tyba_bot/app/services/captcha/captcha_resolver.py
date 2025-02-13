3e001abdb1b13bf97e4d59d7528ac5b3fd951090
Alejandro E Linux <alejandroecheverria.log@gmail.com>
2025-02-12 21:15:35 -0500

    Se agrega atributo pageAction y se deja websiteUrl temporal (pendiente por configurar en .env)

--- bots/tyba_bot/app/services/captcha/captcha_resolver.py
+++ bots/tyba_bot/app/services/captcha/captcha_resolver.py
@@ -7,7 +7,7 @@ from capmonstercloudclient.requests import RecaptchaV3ProxylessRequest
 
 class CaptchaSolver(ABC):
     @abstractmethod
-    async def solve(self, website_url, website_key):
+    async def solve(self, website_url, website_key,page_action):
         """Método abstracto para resolver un CAPTCHA."""
         pass
 
@@ -16,13 +16,14 @@ class ReCaptchaV3Solver(CaptchaSolver):
     def __init__(self):
         self.api_key = CAPMONSTER_KEY
 
-    async def solve(self, website_url: str, website_key: str, min_score: float = 0.5) -> str:
-        client_options = ClientOptions(api_key=self.api_key)
+    async def solve(self, website_url: str, website_key: str, page_action:str,min_score: float = 0.8) -> str:
+        client_options = ClientOptions(api_key=self.api_key,client_timeout=60.0)
         cap_monster_client = CapMonsterClient(options=client_options)
         recaptcha_request = RecaptchaV3ProxylessRequest(
-            websiteUrl=website_url,
+            websiteUrl="https://procesojudicial.ramajudicial.gov.co",
             websiteKey=website_key,
             min_score=min_score,
+            pageAction=page_action
         )
         response = await cap_monster_client.solve_captcha(recaptcha_request)
         return response.get("gRecaptchaResponse", "")
