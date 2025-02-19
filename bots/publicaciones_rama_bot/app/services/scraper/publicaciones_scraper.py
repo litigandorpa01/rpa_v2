238383134aabdd913a596170bc9c1fc6341d53ad
Alejandro E Linux <alejandroecheverria.log@gmail.com>
2025-02-19 16:46:02 -0500

    - Se crea metodo 'get_date_links', es el paso donde se obtiene un diccioanrio que contiene lñas fechas de publicacion y los link de esa fecha

--- bots/publicaciones_rama_bot/app/services/scraper/publicaciones_scraper.py
+++ bots/publicaciones_rama_bot/app/services/scraper/publicaciones_scraper.py
@@ -1,10 +1,10 @@
 import time
-import random
 import asyncio
 import logging
 import traceback
 from datetime import datetime
 from app.constants import WEBSITE_URL
+from datetime import datetime,timedelta
 from app.utils.browser_config import BrowserConfigChrome
 
 from selenium import webdriver
@@ -16,11 +16,12 @@ from selenium.webdriver.support import expected_conditions as EC
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 class PublicacionesScraper:
-    def __init__(self, despa_liti,cod_despacho,ultima_fecha):
+    def __init__(self, despa_liti,cod_despacho,ultima_fecha,interval_days):
         self.website_url = WEBSITE_URL
         self.despa_liti = despa_liti
         self.cod_despacho=cod_despacho
-        self.ultima_fecha=ultima_fecha
+        self.ultima_fecha=datetime.strptime(ultima_fecha, "%d/%m/%Y").date()
+        self.interval_days=int(interval_days)
         self.driver = None
 
     async def configure_driver(self):
@@ -47,53 +48,85 @@ class PublicacionesScraper:
             logging.info(f"❌ Error al iniciar el driver: {e}")
             raise e
         
-    def scipts_config(self):
-        """Aplica una serie de medidas para evitar la detección de Selenium por los sitios web."""
+    def apply_anti_detection_scripts(self):
+        """ Aplica técnicas para evitar la detección de Selenium en la página. """
         try:
-            # Ocultar WebDriver
-            self.driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-
-            # Falsificar permisos del navegador
-            self.driver.execute_script("navigator.permissions.query = (parameters) => Promise.resolve({ state: 'granted' });")
-            
-            # Simular objeto de Chrome
-            self.driver.execute_script("window.chrome = { runtime: {} };")
-
-            # Falsificar propiedades del navegador
-            self.driver.execute_script("""
+            scripts = [
+                "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})",
+                "navigator.permissions.query = (parameters) => Promise.resolve({ state: 'granted' });",
+                "window.chrome = { runtime: {} };",
+                """
                 Object.defineProperty(navigator, 'languages', { get: () => ['en-US', 'en'] });
                 Object.defineProperty(navigator, 'hardwareConcurrency', { get: () => 8 });
                 Object.defineProperty(navigator, 'deviceMemory', { get: () => 8 });
                 Object.defineProperty(navigator, 'maxTouchPoints', { get: () => 1 });
                 Object.defineProperty(navigator, 'platform', { get: () => 'Win32' });
                 Object.defineProperty(navigator, 'vendor', { get: () => 'Google Inc.' });
-            """)
-
-            # Falsificar WebGL y Canvas Fingerprinting
-            self.driver.execute_script("""
+                """,
+                """
                 WebGLRenderingContext.prototype.getParameter = function(parameter) {
                     if (parameter === 37445) return 'Intel Open Source Technology Center';
                     if (parameter === 37446) return 'Mesa DRI Intel(R) HD Graphics 620';
                     return WebGLRenderingContext.prototype.getParameter(parameter);
                 };
-            """)
+                """,
+                "Object.defineProperty(navigator, 'plugins', { get: () => [1, 2, 3, 4, 5] });"
+            ]
 
-            # Deshabilitar `navigator.plugins`
-            self.driver.execute_script("Object.defineProperty(navigator, 'plugins', { get: () => [1, 2, 3, 4, 5] });")
+            for script in scripts:
+                self.driver.execute_script(script)
 
             logging.info("✅ Medidas anti-detección aplicadas con éxito.")
         except Exception as e:
             logging.warning(f"⚠️ Error al aplicar medidas anti-detección: {e}")
+ 
+    def get_date_links(self) -> dict:
+        """ Extrae las publicaciones y filtra por fecha. """
+        try:
+            processal_container = WebDriverWait(self.driver, 3).until(
+                EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'efectosProcesales')]"))
+            )
+            li_elements = processal_container.find_elements(By.XPATH, ".//li[@data-qa-id='row']")
+            
+            data = {}
+            for li in li_elements:
+                try:
+                    publish_date_text = li.find_element(By.XPATH, ".//p[contains(@class, 'publish-date')]").text.strip()
+                    str_date = publish_date_text.split(":")[-1].strip()
+                    publication_date = datetime.strptime(str_date, "%Y-%m-%d").date()
+
+                    urls = [link.get_attribute("href") for link in li.find_elements(By.XPATH, ".//a") if link.get_attribute("href")]
+
+                    if publication_date in data:
+                        data[publication_date].extend(urls)  # Si la fecha ya existe, añade los nuevos links
+                    else:
+                        data[publication_date] = urls  # Si la fecha no existe, crea la entrada
+                    
+                except Exception:
+                    logging.warning("⚠️ No se encontró fecha en un <li>.")
+
+            fecha_inicio = self.ultima_fecha - timedelta(days=self.interval_days)
+            fecha_actual = datetime.today().date()
+            
+            logging.info(fecha_actual)
+            
+            data_filtered = {fecha: urls for fecha, urls in data.items() if fecha_inicio <= fecha <= fecha_actual}
+            
+            print(list(data.keys())) 
+            print(list(data_filtered.keys())) 
+
+            return data_filtered
+        except Exception as e:
+            logging.error(f"❌ Error en `get_date_links`: {e}")
+            raise e
     
     async def run(self):
         """Ejecuta el scraper y maneja la interacción con la página web."""
         try:
             await self.configure_driver()
-
-            #Ingresa a pagina
             self.driver.get(f"{self.website_url}{self.cod_despacho}&_co_com_avanti_efectosProcesales_PublicacionesEfectosProcesalesPortlet_INSTANCE_qOzzZevqIWbb_delta=75")
             time.sleep(1.5)
-            # self.scipts_config()
+            
             legal_office = WebDriverWait(self.driver, 3).until(
                 EC.element_to_be_clickable((By.XPATH, "//span[@id='select2-despacho-container']"))
             )
@@ -102,21 +135,16 @@ class PublicacionesScraper:
             #     print("hola")
             # else:
 
-            processal_container = WebDriverWait(self.driver, 3).until(
-                EC.element_to_be_clickable((By.XPATH, "//div[@class='efectosProcesales lfr-search-container-wrapper main-content-body']"))
-            )
+            data_links = self.get_date_links()
+            logging.info(f"🔍 {len(data_links)} fechas encontradas.")
              
 
-                
-
-
-                    
-    
+        
         except Exception as e:
             logging.error("Ocurrió un error durante la ejecución del scraper:")
             timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
             self.driver.save_screenshot(f"error_{timestamp}.png")
-            traceback.print_exc()
+            logging.error(f"❌ Error durante la ejecución:\n{traceback.format_exc()}")
     
         finally:
             if self.driver:
